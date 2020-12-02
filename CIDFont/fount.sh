b1e2387577af53afb1bfd38b17e0bc73699ff6bb
蒋阳 <978338340@qq.com>
2020-12-02 10:56:19 +0800

    添加依赖文件

---
+++ CIDFont/fount.sh
@@ -0,0 +1,3 @@
+sudo cp AdobeHeitiStd-Regular.otf /usr/share/ghostscript/9.26/Resource/CIDFont/AdobeHeitiStd-Regular
+sudo cp AdobeSongStd-Light.otf /usr/share/ghostscript/9.26/Resource/CIDFont/AdobeSongStd-Light
+sudo update-gsfontmap
